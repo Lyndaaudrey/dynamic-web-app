64c90f547d99e60f53c773d061b0b590b2006b4f
Lyndaaudrey <lynda-audrey.aka-ngweyeno@etu.umontpellier.fr>
2023-12-08 19:14:32 +0100

    Test 2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -24,21 +24,26 @@ variable "private_key" {}
 
 variable "key_name" {
   type = string 
+  default = "project_auth"
 }
 
 
 variable "bucket_name" {
   type = string
+  default = "dynamic-web-app-state-lock"
 }
 
 variable "dynamodb_table" {
   type = string
+  default = "terraform-state-lock"
 }
 
 variable "region" {
   type = string
+  default = "us-east-1"
 }
 
 variable "bucket_key" {
   type = string
+  default = "backend/dynamic-website.tfstate"
 }
